79589724acb57345d83f09610cfa62b4c4362665
Bizet Rodriguez <brodriguez-velez@ucmerced.edu>
2018-11-14 16:22:30 -0800

    Initial Commit

---
+++ onBoardTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  onBoardTest
+//
+//  Created by Bizet Rodriguez on 11/14/18.
+//  Copyright © 2018 Bizet Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
